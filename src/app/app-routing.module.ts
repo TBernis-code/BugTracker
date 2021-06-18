942bb484edc6cfbdb1bd3cd62ad81792dfd74ffc
Tony Bernis <tony.bernis@kdetude.com>
2021-06-18 12:33:48 +0200

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormtestComponent } from './formtest/formtest.component';
 import { BugComponent } from './bug/bug.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes: Routes = [
   { path: 'bug-form', component: FormtestComponent },
   { path: 'bug-list', component: BugComponent },
+  { path: 'home', component: NavBarComponent },
 ];
 
 @NgModule({
